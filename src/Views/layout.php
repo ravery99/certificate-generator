4cbdcf0a6bf031260fc532c59d8d126ac96dc6ad
bella <bell.isa0104@gmail.com>
2025-02-19 16:32:35 +0700

    fix: add layout

---
+++ src/Views/layout.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> <?php echo $title ?> </title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body {
+            background-color: #f9fafb; 
+
+        }
+    </style>
+</head>
+
+<body class="flex justify-center items-center min-h-screen p-4 sm:p-8 overscroll-none">
+
+    <?php  require_once "../src/Views/$viewPath.php"; ?> 
+
+</body>
+
+</html>
